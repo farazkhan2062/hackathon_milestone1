c8f21f2d2c65d1fc69740ad2d38a78bbd6f08b1e
farazkhan2062 <farazkhan2062@gmail.com>
2024-11-10 21:28:55 +0500

    Initial Commit
    Hackathon first Milestone

---
+++ app.js
@@ -0,0 +1,37 @@
+"use strict";
+// =------- Contact section----------------
+let cont_section = document.getElementById("contact");
+let cont_btn = document.getElementById("cont_btn");
+cont_btn?.addEventListener("click", () => {
+    cont_section.classList.toggle("hide");
+});
+// =------- Education section----------------
+let edu_section = document.getElementById("education");
+let edu_btn = document.getElementById("edu_btn");
+edu_btn?.addEventListener("click", () => {
+    edu_section.classList.toggle("hide");
+});
+// =------- Expertise section----------------
+let expt_section = document.getElementById("expertise");
+let expt_btn = document.getElementById("expt_btn");
+expt_btn?.addEventListener("click", () => {
+    expt_section.classList.toggle("hide");
+});
+// =------- Language section----------------
+let lang_section = document.getElementById("language");
+let lang_btn = document.getElementById("lang_btn");
+lang_btn?.addEventListener("click", () => {
+    lang_section.classList.toggle("hide");
+});
+// =------- Experince section----------------
+let exp_section = document.getElementById("experince");
+let exp_btn = document.getElementById("exp_btn");
+exp_btn?.addEventListener("click", () => {
+    exp_section.classList.toggle("hide");
+});
+// =------- Refrence section----------------
+let ref_section = document.getElementById("ref");
+let ref_btn = document.getElementById("ref_btn");
+ref_btn?.addEventListener("click", () => {
+    ref_section.classList.toggle("hide");
+});
